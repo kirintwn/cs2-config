4eea78d8b348aeaa74761977c7eea82c71657026
kirintwn <me@kirintw.me>
2023-11-17 14:22:54 +0800

    chore: clean up & adjust player mark color

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 SRC_DIR="/home/kirintw/Documents/works/personal/cs2-config/cfg"
-DST_DIR="/run/media/kirintw/data/games/steam/steamapps/common/Counter-Strike Global Offensive/game/csgo/cfg"
+DST_DIR="/run/media/kirintw/data/SteamLibrary/steamapps/common/Counter-Strike Global Offensive/game/csgo/cfg"
 
 rsync -avhW --no-compress --progress "$SRC_DIR/" "$DST_DIR"
